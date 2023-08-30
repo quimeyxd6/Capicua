3332f18cc5c3ce70a2af192366c49843c9a106ee
quimeyxd6 <quimeyxd6@gmail.com>
2023-08-29 22:53:23 -0300

    Corrección en boton Almacenar

--- custom.js
+++ custom.js
@@ -1,22 +1,25 @@
 'use strict'
 
 function almacenarPalabra() {
-    var palabra = document.getElementById('palabra').value.toLowerCase();
-    palabra = palabra.replace(/[^a-z0-9]/g, "");
+    var palabra = document.getElementById('palabra').value.toLowerCase().replace(/[^a-z]/g);
     var longitud = palabra.length;
-    var esCapicúa = true;
 
-    console.log(palabra);
-    console.log(longitud);
-    for (var i = 0; i < Math.floor(longitud / 2); i++) {
-        if (palabra[i] !== palabra[longitud - 1 - i]) {
-            esCapicúa = false;
-            break;
+        if(palabra == '') {
+            document.getElementById("palabraAlmacenada").textContent = 'Falta ingresar palabra';
+        } else {
+            var esCapicúa = true;
+            for (var i = 0; i < Math.floor(longitud / 2); i++) {
+                if (palabra[i] !== palabra[longitud - 1 - i]) {
+                    esCapicúa = false;
+                    break;
+                }
+            }
+
+            if (esCapicúa) {
+                document.getElementById("palabraAlmacenada").textContent = 'Es Capicúa';
+            } else {
+                document.getElementById("palabraAlmacenada").textContent = 'No es Capicúa';
+            }
         }
     }
-    if (esCapicúa) {
-        document.getElementById("palabraAlmacenada").textContent = 'Es Capicúa';
-    } else {
-        document.getElementById("palabraAlmacenada").textContent = 'No es Capicúa';
-    }
-}
+    console.log(palabra);
